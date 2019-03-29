39a8b6ef97172e205cafc58a59d33de244116a07
Gogs <gogs@fake.local>
2019-03-29 17:19:40 +0800

    chapter7.1

---
+++ practice/chapter7/q71/question.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := [...]string{"a", "b", "c", "d", "e"} //...表示根据实际传入的参数设置length
+	for i := range a {
+		fmt.Println("Array item", i, "is", a[i])
+	}
+
+	println(len(a))
+	//a[5] = "f" //由于长度是五，这地方会报数组角标越界
+
+}
