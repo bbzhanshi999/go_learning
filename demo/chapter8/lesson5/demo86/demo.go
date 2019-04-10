de42da54fd6e3197e330fc1d518e26b6c7fa9955
Gogs <gogs@fake.local>
2019-04-10 11:50:09 +0800

    finsih a

--- demo/chapter8/lesson5/demo86/demo.go
+++ demo/chapter8/lesson5/demo86/demo.go
@@ -5,6 +5,7 @@ import (
 	"sort"
 )
 
+//构造一个将英文饮料名映射为法语（或者任意你的母语）的集合；先打印所有的饮料，然后打印原名和翻译后的名字。接下来按照英文名排序后再打印出来。
 func main() {
 	var barVal = map[string]int{
 		"alpha": 34, "bravo": 56, "charlie": 23,
